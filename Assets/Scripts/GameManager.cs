f19fdbeaddbc354b5ac2a333277c24d7742b86c2
Takuuuu228 <chackyken456@gmail.com>
2024-02-24 01:18:54 +0900

    ランダムでどんどん文字が入れ替わる

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,28 +4,13 @@ using UnityEditor; // UnityEditorを使用するために必要
 public class GameManager : MonoBehaviour
 {
     // インスペクター上で設定可能な単語を保持するための変数を定義する
-    public WordPairGroup[] wordPairGroups;
-    public int magnification;
+    public WordPair[] wordPairs;
 
     private int score = 0; // スコアを格納するための変数を定義する
 
     void Start()
     {
-        // スコアを計算する
-        CalculateScore();
-    }
 
-    // スコアを計算するメソッド
-    private void CalculateScore()
-    {
-        // 各単語グループの文字数に応じてスコアを加算する
-        foreach (var group in wordPairGroups)
-        {
-            foreach (var pair in group.wordPairs)
-            {
-                score += pair.japanese.Length * magnification; // ローマ字と日本語の文字数を加算する
-            }
-        }
     }
 
     // 単語とその日本語訳を保持するクラスを定義する
@@ -34,12 +19,5 @@ public class GameManager : MonoBehaviour
     {
         public string romaji; // ローマ字の単語を保持する変数
         public string japanese; // 日本語の訳を保持する変数
-    }
-
-    // 単語グループを保持するクラスを定義する
-    [System.Serializable]
-    public class WordPairGroup
-    {
-        public WordPair[] wordPairs; // 単語とその日本語訳を保持する配列
     }
 }
--- Assets/Scripts/TypeManager.cs
+++ Assets/Scripts/TypeManager.cs
@@ -1,61 +1,61 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class TypeManager : MonoBehaviour
 {
-    public Text nameDisplay;
-    public Text stringsDisplay;
-    private List<GameManager.WordPair> wordPairs = new List<GameManager.WordPair>();
-    private string currentRomaji = ""; // 現在入力中のローマ字文字列を保持する変数
+    // GameManagerへの参照
+    public GameManager gameManager;
+
+    public Text japaneseText;
+    public Text romajiText;
+
+    // ランダムで取得したローマ字と日本語のペアを保持する変数
+    private string currentRomaji;
+    private string currentJapanese;
 
     void Start()
     {
-        GameManager gameManager = FindObjectOfType<GameManager>();
-        if (gameManager != null && gameManager.wordPairGroups.Length > 0)
-        {
-            List<GameManager.WordPair> availablePairs = new List<GameManager.WordPair>(gameManager.wordPairGroups[0].wordPairs);
-            for (int i = 0; i < Mathf.Min(3, availablePairs.Count); i++)
-            {
-                int randomIndex = Random.Range(0, availablePairs.Count);
-                wordPairs.Add(availablePairs[randomIndex]);
-                availablePairs.RemoveAt(randomIndex);
-            }
-        }
-
-        DisplayWordPairs();
+        // 最初にランダムな単語を取得してテキストに表示する
+        GetRandomWordPair();
+        UpdateTextDisplay();
     }
 
     void Update()
     {
-        // キーボードからの入力を監視する
+        // キーボードからの入力を取得する
         if (Input.anyKeyDown)
         {
-            // 入力された文字を取得
-            string input = Input.inputString;
-
-            // 現在のローマ字文字列との比較
-            if (wordPairs.Count > 0 && wordPairs[0].romaji.StartsWith(input))
+            // 入力された文字と現在のローマ字の先頭文字が一致するかチェックする
+            if (Input.inputString.Equals(currentRomaji.Substring(0, 1)))
             {
-                // 入力された文字列がローマ字文字列の先頭と一致する場合、ローマ字文字列の先頭1文字を削除
-                wordPairs[0].romaji = wordPairs[0].romaji.Remove(0, 1);
+                // 先頭の文字を消去する
+                currentRomaji = currentRomaji.Substring(1);
+                UpdateTextDisplay(); // テキスト表示を更新する
             }
 
-            // テキスト表示を更新
-            DisplayWordPairs();
+            // もしローマ字の文字列が空になったら
+            if (currentRomaji.Length == 0)
+            {
+                // 新しい単語を取得してテキストに表示する
+                GetRandomWordPair();
+                UpdateTextDisplay();
+            }
         }
     }
 
-    private void DisplayWordPairs()
+    // ランダムな単語をGameManagerから取得するメソッド
+    private void GetRandomWordPair()
     {
-        string nameText = "";
-        string stringsText = "";
-        foreach (var wordPair in wordPairs)
-        {
-            nameText += wordPair.japanese + "\n";
-            stringsText += wordPair.romaji + "\n"; // ローマ字文字列のみを表示
-        }
-        nameDisplay.text = nameText;
-        stringsDisplay.text = stringsText;
+        // GameManagerからランダムな単語を取得する
+        int randomIndex = Random.Range(0, gameManager.wordPairs.Length);
+        currentRomaji = gameManager.wordPairs[randomIndex].romaji;
+        currentJapanese = gameManager.wordPairs[randomIndex].japanese;
+    }
+
+    // テキスト表示を更新するメソッド
+    private void UpdateTextDisplay()
+    {
+        romajiText.text = currentRomaji; // ローマ字のテキストを更新する
+        japaneseText.text = currentJapanese; // 日本語のテキストを更新する
     }
 }
